e3389c917f0eba63a835cf9ae370fd80d3165541
cchall <chall@radiasoft.net>
2025-10-28 14:48:48 -0400

    Add vertice calc to element

--- collider/element.py
+++ collider/element.py
@@ -9,6 +9,10 @@ class Element:
         self.width = width
         self.interactions = 0
 
+        # half widths are used internally to cut down on operations
+        self._hwx = width[0]/2.
+        self._hwy = width[1]/2.
+
     @property
     def cx(self) -> float:
         return self.center[0]
@@ -21,3 +25,22 @@ class Element:
     @property
     def wy(self) -> float:
         return self.width[1]
+
+    @property
+    def vertices(self) -> np.ndarray:
+        """Returns vertices in clockwise direction.
+                v1 --------------- v2
+                 |                  |
+                 |        (0,0)     |
+                 |         +        |
+                 |                  |
+                 |                  |
+                v4 --------------- v3
+        """
+        v1 = [self.cx - self._hwx, self.cy + self._hwy]
+        v2 = [self.cx + self._hwx, self.cy + self._hwy]
+        v3 = [self.cx + self._hwx, self.cy - self._hwy]
+        v4 = [self.cx - self._hwx, self.cy - self._hwy]
+
+        return np.array([v1, v2, v3, v4])
+

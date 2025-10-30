7bd17d7064528c54d5b62e91fd6ce8a74a84b32c
cchall <chall@radiasoft.net>
2025-10-30 11:41:35 -0400

    Overlap detection with angles finished

--- collider/actions.py
+++ collider/actions.py
@@ -1,24 +1,24 @@
 from collider import beam
 from collider import element
+from collider.geometry import rotate_element_vertices
 import math
 import numpy as np
 
 
-def rotation_matrix(angle: float) -> np.ndarray:
-    angle_radians = np.radians(angle)
-    rotation_matrix = np.array([
-        [np.cos(angle_radians), -np.sin(angle_radians)],
-        [np.sin(angle_radians), np.cos(angle_radians)]
-    ])
-    return rotation_matrix
-
-
-def update_overlap(beam1: beam.Beam, beam2: beam.Beam):
+def update_overlap(beam1: beam.Beam, beam2: beam.Beam) -> None:
+    """Count all overlapping elements between two beams. Updates interaction counters in place.
+    (!) This function assumes all beam elements are the same width and height.
+    """
+    # Find extrema of each beam - all elements are the same size so we can check this for only one element
     largest_edge1 = max(beam1.elements[0].wx, beam1.elements[0].wy) / 2.
     smallest_edge1 = min(beam1.elements[0].wx, beam1.elements[0].wy) / 2.
     largest_edge2 = max(beam2.elements[0].wx, beam2.elements[0].wy) / 2.
     smallest_edge2 = min(beam2.elements[0].wx, beam2.elements[0].wy) / 2.
 
+    # Perform a fast check if element centers are close enough to overlap
+    # If they are not then continue
+    # If they are under the length of the smallest edges there must be overlap
+    # If we fall between these two extremes a full check of all projections for overlap is required
     for e1 in beam1.elements:
         for e2 in beam2.elements:
             c2c_dist = math.sqrt((e1.cx - e2.cx)**2 + (e1.cy - e2.cy)**2)
@@ -28,29 +28,25 @@ def update_overlap(beam1: beam.Beam, beam2: beam.Beam):
                 e1.interactions += 1
                 e2.interactions += 1
             else:
-                # TODO: sat_overlap_detection will run here
-                pass
+                overlap = elements_overlap(e1, e2, beam1.angle, beam2.angle)
+                if overlap:
+                    e1.interactions += 1
+                    e2.interactions += 1
 
 
-# TODO: Either implement angle and rotate the elements prior to check or pass in global coordinates so no rotation is needed
 def elements_overlap(element1: element.Element, element2: element.Element, angle1: float, angle2: float) -> bool:
     """Returns True if the elements overlap, otherwise False"""
-    vertices = []
-    for ele, angle in zip((element1, element2), (angle1, angle2)):
-        if angle != 0.:
-            vertices.append(np.dot(ele.vertices, rotation_matrix(angle)))
-        else:
-            vertices.append(ele.vertices)
-    vertices1, vertices2 = vertices[0], vertices[1]
+    vertices1 = rotate_element_vertices(element1, angle1, centered=True)
+    vertices2 = rotate_element_vertices(element2, angle2, centered=True)
 
     # For each rectangle (element) we find the two orthogonal edges (p)
     edges1 = [  # indexing: [edge][vertex][coordinate]
-        [element1.vertices[0], element1.vertices[1]],
-        [element1.vertices[1], element1.vertices[2]],
+        [vertices1[0], vertices1[1]],
+        [vertices1[1], vertices1[2]],
     ]
     edges2 = [ # indexing: [edge][vertex][coordinate]
-        [element2.vertices[0], element2.vertices[1]],
-        [element2.vertices[1], element2.vertices[2]],
+        [vertices2[0], vertices2[1]],
+        [vertices2[1], vertices2[2]],
     ]
 
     # For each edge we find the unit vector along that edge. This unit vector
@@ -60,11 +56,11 @@ def elements_overlap(element1: element.Element, element2: element.Element, angle
     ]
     projectsion2 = [  # indexing = [projection][coordinate]
         (edge[1][0] - edge[0][0], edge[1][1] - edge[0][1]) for edge in edges2
-    ]  # TODO: Trying without making unit vector, need: / math.sqrt(edge11[1][0]**2 + edge11[1][1]**2)
+    ]
 
     for proj in (*projections1, *projectsion2):  # 4 total projections to check
-        ele1_proj = [np.dot(proj, v) for v in element1.vertices]
-        ele2_proj = [np.dot(proj, v) for v in element2.vertices]
+        ele1_proj = [np.dot(proj, v) for v in vertices1]
+        ele2_proj = [np.dot(proj, v) for v in vertices2]
 
         # If cond1 is true ele1 is fully to the right of ele2 or if cond2 then it is fully to the left.
         # Otherwise, the two elements overlap on this projection.
@@ -75,19 +71,19 @@ def elements_overlap(element1: element.Element, element2: element.Element, angle
     return True
 
 
-
-
-
 if __name__ == "__main__":
+    # Test
     import matplotlib.pyplot as plt
     element1 = element.Element(center=(0.5, 0.24), width=(1.3, 0.89))
     element2 = element.Element(center=(-0.8, 0.24), width=(2.24, 0.1))
+    angle1 = 0
+    angle2 = 30
 
-    overlap = elements_overlap(element1, element2, angle1=0, angle2=0)
+    overlap = elements_overlap(element1, element2, angle1=angle1, angle2=angle2)
     plt.figure()
-    for v in element1.vertices:
+    for v in rotate_element_vertices(element1, angle1, centered=True):
         plt.scatter(*v, c='C1')
-    for v in element2.vertices:
+    for v in rotate_element_vertices(element2, angle2, centered=True):
         plt.scatter(*v, c='C2')
     plt.savefig('overlap.png')
     print(f"{overlap=}")

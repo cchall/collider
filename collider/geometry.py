9509d9b0125ed3faf1f88d67c5cda480efbc15e4
cchall <chall@radiasoft.net>
2025-10-30 11:41:52 -0400

    Add helper function module for manipulating elements

---
+++ collider/geometry.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def rotation_matrix(angle: float) -> np.ndarray:
+    angle_radians = np.radians(angle)
+    rotation_matrix = np.array([
+        [np.cos(angle_radians), -np.sin(angle_radians)],
+        [np.sin(angle_radians), np.cos(angle_radians)]
+    ])
+    return rotation_matrix
+
+
+def rotate_element_vertices(element, angle: float, centered: bool = True) -> np.ndarray:
+    if centered:
+        vertices = element.vertices.copy()
+        vertices[:, 0] -= element.cx
+        vertices[:, 1] -= element.cy
+        vertices = np.dot(vertices, rotation_matrix(angle))
+        vertices[:, 0] += element.cx
+        vertices[:, 1] += element.cy
+    else:
+        vertices = np.dot(element.vertices, rotation_matrix(angle))
+
+    return vertices

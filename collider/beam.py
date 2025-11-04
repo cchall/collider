619584e181d6b5c0f50e0943c6243696fd414e47
cchall <chall@radiasoft.net>
2025-11-04 18:40:08 -0500

    Add coloring by interaction count to beam plot

--- collider/beam.py
+++ collider/beam.py
@@ -66,10 +66,38 @@ class Beam:
         else:
             fig, ax = figure
 
-        outline_patch = plt.Rectangle((self.Cx - self.Lx/2., self.Cy - self.Ly/2.), self.Lx, self.Ly,
-                                      facecolor=None, edgecolor='black', alpha=0.2,
+        outline_patch = plt.Rectangle((self.Cx - self.Lx / 2., self.Cy - self.Ly / 2.), self.Lx, self.Ly,
+                                      facecolor='none', edgecolor='black', alpha=0.2,  # 'none' is standard for no fill
                                       rotation_point='center', angle=self.angle)
         ax.add_patch(outline_patch)
+
+        # --- Start of Modifications ---
+
+        # 1. Check if there are elements to plot
+        if not self.elements:
+            # If no elements, just set limits, save, and return
+            ax.set_xlim(-self.Lx * 2, self.Lx * 2)
+            ax.set_ylim(-self.Lx * 2, self.Lx * 2)
+            plt.savefig('beam.png')
+            return fig, ax
+
+        # 2. Get the data range for the color scale
+        interactions = [ele.interactions for ele in self.elements]
+        min_val = min(interactions)
+        max_val = max(interactions)
+
+        # 3. Create a normalizer and a colormap
+        # Use plt.Normalize to map the interaction values to the [0, 1] range
+        norm = plt.Normalize(vmin=min_val, vmax=max_val)
+
+        # Choose a colormap (e.g., 'viridis', 'plasma', 'inferno', 'jet', 'coolwarm')
+        cmap = plt.cm.get_cmap('viridis')
+
+        # 4. Create a ScalarMappable to handle color mapping and for the colorbar
+        mappable = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+
+        # --- End of Modifications ---
+
         for ele in self.elements:
             x_center = ele.cx
             y_center = ele.cy
@@ -79,12 +107,23 @@ class Beam:
             x_center_rot += self.Cx
             y_center_rot += self.Cy
 
-            patch = plt.Rectangle((x_center_rot - self.dx/2., y_center_rot - self.dy/2.), ele.wx, ele.wy,
+            # --- Modified facecolor ---
+            # Get the RGBA color for this element's interaction value
+            color = mappable.to_rgba(ele.interactions)
+
+            patch = plt.Rectangle((x_center_rot - self.dx / 2., y_center_rot - self.dy / 2.), ele.wx, ele.wy,
                                   rotation_point='center', angle=self.angle,
-                                  facecolor='C4', edgecolor='black', alpha=0.2)
+                                  facecolor=color, edgecolor='black',
+                                  alpha=0.2)  # Your original alpha is preserved
             ax.add_patch(patch)
-        ax.set_xlim(-self.Lx*2, self.Lx*2)
-        ax.set_ylim(-self.Lx*2, self.Lx*2)
+
+        # 5. Add the colorbar to the figure
+        cbar = fig.colorbar(mappable, ax=ax, orientation='vertical')
+        cbar.set_label('Interactions')  # Set a label for the colorbar
+
+        ax.set_xlim(-(self.Cx + self.Lx) * 2, (self.Cx + self.Lx)  * 2)
+        ax.set_ylim(-(self.Cy + self.Ly)  * 2, (self.Cy + self.Ly)  * 2)
+        ax.set_aspect('equal')
         plt.savefig('beam.png')
 
         return fig, ax

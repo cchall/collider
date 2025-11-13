30bdf46cb84946f03a311a268e65bb0c4395af8e
cchall <chall@radiasoft.net>
2025-11-12 21:15:20 -0500

    transform if needed

--- collider/propagate.py
+++ collider/propagate.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from collider.beam import Beam
 
 
@@ -5,3 +7,13 @@ def propagate(beam: Beam, time_step: float) -> Beam:
     beam.update_position(time_step)
 
     return beam
+
+def lorentz_transform(four_vector: np.ndarray, beta_vector: np.ndarray, gamma: float) -> np.ndarray:
+    v0, v = four_vector[0], four_vector[1:]
+
+    beta_dot_v = np.dot(beta_vector, v)
+    v0_p = gamma * (v0 - beta_dot_v)
+
+    v_p = v + beta_vector * (gamma ** 2 * beta_dot_v / (gamma + 1) - gamma * v0)
+
+    return np.array([v0_p, *v_p])
